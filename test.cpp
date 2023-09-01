1642e27e72c77eb009c956beddff6f52e95bf199
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-09-01 18:30:41 +0530

    -Time: 7 ms (98.81%), Space: 16.9 MB (68.83%)

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Insert function here
+
+int main() {
+
+   vector<int> ans= countBits(5);
+
+    for (int i : ans ) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
