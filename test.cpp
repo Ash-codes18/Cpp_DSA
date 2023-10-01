c3e4ee7ab1066ecb93c27ace5c1b126ff55dc0e2
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-10-01 22:54:38 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (11.69%)

--- test.cpp
+++ test.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-//Insert function here
+// Insert function here
+ vector<int> plusOne(vector<int>& digits) {
 
-int main() {
+        int n=digits.size();
+        int carry=1;
 
-   vector<int> ans= countBits(5);
+        for(int i=n-1;i>=0;i--){
 
-    for (int i : ans ) {
-        cout << i << " ";
+            if(i==0 && digits[i]==9){
+                digits.insert(digits.begin(),1);
+                digits[i+1]=0;
+            }
+
+                else if(digits[i]==9){
+                     digits[i]=0;
+                }
+                else{
+                     digits[i]+=1;
+                     break;
+                }
+        }
+        return digits;
     }
+
+int main()
+{
+    vector<int> v{9};
+    plusOne(v);
+
+    for(int i:v){
+        cout<<i<<" ";
+    }
+
 
     return 0;
 }
