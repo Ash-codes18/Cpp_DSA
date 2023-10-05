1e613e7987402efa18448a24ce13212aee0cb034
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-10-05 16:29:13 +0530

    delete 10 files, move 2 files and update 1 file

--- test.cpp
+++ test.cpp
@@ -5,61 +5,32 @@
 using namespace std;
 
 // Insert function here
-
-
-int findmax(int arr[], int n){
-    int max=arr[0];
-    int index=0;
-    for(int i=0;i<n;i++){
-        if(max<arr[i]){
-        max=arr[i];
-        index=i;
-        }
-    }
-    return index;
-}
-
-
-char find_duplicates(int arr[],string s, int n, int index){
-    vector<int> indices;
-    for(int i=0;i<n;i++){
-        if(arr[i]==arr[index]){
-            indices.push_back(i);
-        }
-    }
-
-    char ans=s[index];
-
-    for(int i=0;i<indices.size();i++){
-        if (ans>s[indices[i]]){
-            ans = s[indices[i]];
-        }
-    }
-    return ans;
-
-}
-
-    //Function to find the maximum occurring character in a string.
     char getMaxOccuringChar(string str)
     {
         // Your code here
-        int n=str.length();
-        int arr[n]={0};
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && str[i]==str[j]){
-                    arr[i]++;
-                }
-            }
+        int arr[26]={0};
+
+        for(int i=0;i<str.length();i++){
+            arr[str[i]-'a']++;
         }
 
-        int max_index=findmax(arr,n);
+        int max=0;
+        char ans='a';
+        
+        for(int i=0;i<26;i++){
 
-        char dups=find_duplicates(arr,str,n,max_index);
+             if(arr[i]>max){
+                max=arr[i];
+                ans=i+'a';
+            }
 
+            else if(arr[i]==max && i+'a'<ans){
+                    ans=i+'a';
+            }
 
-return dups;
+        }
+        return ans;
 
     }
 
@@ -69,7 +40,8 @@ return dups;
 int main()
 {
     // vector<int> v{9,6,4,2,3,5,7,0,1};
-    cout<<getMaxOccuringChar("vhqvoi");
+    char a=getMaxOccuringChar()");
+    cout<<a<<endl;
 
     // for(int i:v){
     //     cout<<i<<" ";
