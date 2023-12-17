d5956a474fb503d4fbadc02e17af43eddd436a9d
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-12-17 08:21:24 +0530

    Time: 2 ms (75.01%), Space: 6.2 MB (79.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int fib(int n) {
-    if(n==0){
-        return 0;
-    }
-
-    if(n==1){
-        return 1;
-    }
     
-    return fib(n-1)+fib(n-2);
+    if(n<=1){
+        return n;
+    }
+        
+    int a=0;
+    int b=1;
+    int sum;
+        
+    n-=1;
+        
+    while(n--){
+        sum=a+b;
+        a=b;
+        b=sum;
+    }
+        
+    return sum;
+    
     }
 };
