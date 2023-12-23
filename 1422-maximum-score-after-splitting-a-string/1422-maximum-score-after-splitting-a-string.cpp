b2024709fc09f9b008cbf7b09099f3d0bb7af642
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-12-23 19:50:09 +0530

    Time: 11 ms (5.75%), Space: 14.2 MB (5.75%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+int countzeros(string s, int end){
+    int count=0;
+    while(end--){
+        if(s[end]=='0'){
+            count++;
+        }
+    }
+    return count;
+}
+
+int countones(string s, int start, int end){
+    int count=0;
+    while(start<end){
+        if(s[start++]=='1'){
+            count++;
+        }
+    }
+    return count;
+}
+
+    int maxScore(string s) {
+        int n=s.length();
+        int ones,zeros;
+        int maxscore=INT_MIN;
+        string left,right;
+
+
+        for(int i=0;i<n-1;i++){
+            zeros=countzeros(s,i+1);
+            ones=countones(s,i+1,n);
+            if(ones+zeros>maxscore){
+                maxscore=ones+zeros;
+            }
+        }
+
+        return maxscore;
+
+    }
+};
