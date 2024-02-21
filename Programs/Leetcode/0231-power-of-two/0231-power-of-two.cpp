d53008b13e1ee994974041ed4971ab8fb8ec81a7
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-02-21 14:16:47 +0530

    delete 2 files, move 17 files and update 2 files

--- Programs/Leetcode/0231-power-of-two/0231-power-of-two.cpp
+++ Programs/Leetcode/0231-power-of-two/0231-power-of-two.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       int i = 0;
-    bool flag = false;
-    while (i <=30)
-    {
-        int checker = pow(2, i);
-        if (checker == n)
-        {
-            flag = true;
-            break;
+        int i = 0;
+        int checker = 0;
+
+        while (1) {
+            if (checker > n || i > 30) {
+                return false;
+            }
+            checker = pow(2, i);
+            if (checker == n) {
+                return true;
+            }
+            i++;
         }
-        i++;
-    }
-    if (flag)
-    {
-         return true;
-    }
-    else
-    {
-        return false;
-    }
     }
 };
--- Programs/Love Babbar/PHASE-II/OOPS/static.cpp
+++ Programs/Love Babbar/PHASE-II/OOPS/static.cpp
@@ -35,3 +35,60 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int a = 0 , j = k;
+        int ct = 0;
+        //check for vowels in first k elements
+        for(int i=0;i<k;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                ct++;
+            }
+        }
+        int maxi = ct;
+        //if ct is equal to k , there couldn't be greater answer than that so return 
+        if(ct==k) return ct;
+        //now keep moving the window of size k by increamenting j and a as a++ and j++
+        while(j<s.size()){
+            
+            //if previous first element is vowel then decrease it's count
+            if(s[a]=='a' || s[a]=='e' || s[a]=='i' || s[a]=='o' || s[a]=='u'){
+                ct--;
+            }
+            a++;
+            //if upcoming element is vowel then increase the count
+            if(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u'){
+                
+               ct++;
+            }
+            j++;
+            //store the maximum count till now
+            maxi = max(maxi,ct);
+            
+        }
+        return maxi;
+    }
+};
