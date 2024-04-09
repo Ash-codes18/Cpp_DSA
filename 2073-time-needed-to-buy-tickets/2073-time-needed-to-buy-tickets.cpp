dea0a0d785724d7b56c9b66be5b407dc51ff1ee5
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-04-09 11:01:27 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (67.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,3 +1,8 @@
+// Approach-1 -> O(n^2)
+
+
+
+/*
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
@@ -24,6 +29,40 @@ public:
 
 return count;
 
+
+    }
+};
+
+*/
+
+
+// Approach-2 -> O(n)
+
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        
+        int n = tickets.size();
+        int count=0;
+        int val = tickets[k];
+
+
+        for(int i=0;i<n;i++){
+            if(i<k && tickets[i]>=val){
+                count+=val;
+            }
+            else if(i>k && tickets[i]>=val){
+                count+=val-1;
+            }
+            else if(tickets[i]<val){
+                count+=tickets[i];
+            }
+        }
+        
+
+
+    return count+val;
 
     }
 };
