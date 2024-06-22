0689f254c1c514104a0788a433cfe72ca86b6e81
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-06-22 13:37:31 +0530

    Time: 23 ms (82.62%), Space: 30.5 MB (57.95%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,49 +1,77 @@
+// Approach-1 : Using Sliding Window 
+
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int n = nums.size();
+//         int len = INT_MAX;
+//         int sum = 0, count = 0, j = 0, s = 0;
+
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//             j++;
+//             count++;
+//             if(sum>=target){
+//                 len = min(len,count);
+//                 break;
+//             }
+//         }
+
+//         while(j<n){
+//             cout<<sum<<endl;
+//             if(sum>target){
+//                 sum-=nums[s++];
+//                 count--;
+//                 if(sum>=target){
+//                     len = min(len,count);
+//                 }
+//             }
+//             else if(sum<target){
+//                 sum+=nums[j++];
+//                 count++;
+//             }
+//             else{
+//                 sum-=nums[s++];
+//                 sum+=nums[j++];
+//                 len = min(len,count);
+//             }
+//         }
+
+//         while(s<n-1 && sum>=target){
+//             sum-=nums[s++];
+//             count--;
+//             if(sum>=target){
+//                 len = min(len,count);
+//             }
+//         }
+
+
+// return (len==INT_MAX)?0:len;
+
+
+//     }
+// };
+
+
+//  Approach-2 : Using 2 Pointers
+
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+
         int n = nums.size();
         int len = INT_MAX;
-        int sum = 0, count = 0, j = 0, s = 0;
+        int sum = 0, count = 0,i = 0, j = 0;
 
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            j++;
-            count++;
-            if(sum>=target){
-                len = min(len,count);
-                break;
+        while(i<n){
+            sum+=nums[i++];
+            while(sum>=target){
+                len=min(len,i-j);
+                sum-=nums[j++];
             }
         }
 
-        while(j<n){
-            if(sum>target){
-                sum-=nums[s++];
-                count--;
-                if(sum>=target){
-                    len = min(len,count);
-                }
-            }
-            else if(sum<target){
-                sum+=nums[j++];
-                count++;
-            }
-            else{
-                sum-=nums[s++];
-                sum+=nums[j++];
-                len = min(len,count);
-            }
-        }
-
-        while(s<n-1 && sum>=target){
-            sum-=nums[s++];
-            count--;
-            if(sum>=target){
-                len = min(len,count);
-            }
-        }
-
-
-return (len==INT_MAX)?0:len;
+return (len==INT_MAX)? 0:len;
 
 
     }
