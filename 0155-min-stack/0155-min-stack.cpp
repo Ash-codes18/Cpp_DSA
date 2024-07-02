36cba10529be38d95e4feb516ff197efbbef53be
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-02 21:23:32 +0530

    Time: 20 ms (35.22%), Space: 19.9 MB (93.24%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,48 +1,108 @@
+// Using Extra space for returning min element
+
+// class MinStack {
+// public:
+//     vector<int> ele;
+//     vector<int> currmin;
+//     int tempmin;
+//     int ind;
+
+//     MinStack() {
+//         tempmin=INT_MAX;
+//         ind=-1;
+//     }
+    
+//     void push(int val) {
+//         ind++;
+//         ele.push_back(val);
+//         tempmin=min(val,tempmin);
+//         currmin.push_back(tempmin);
+//     }
+    
+//     void pop() {
+//         ele.pop_back();
+//         currmin.pop_back();
+//         ind--;
+//         if(ind<0){
+//             tempmin=INT_MAX;
+//         }
+//         else{
+//             tempmin = currmin[ind];
+//         }
+//     }
+    
+//     int top() {
+//         return ele[ind];
+//     }
+    
+//     int getMin() {
+//         return currmin[ind];
+//     }
+// };
+
+
+
+// Without using extra space for returning the min element
+
 class MinStack {
 public:
-    vector<int> ele;
-    vector<int> currmin;
-    int tempmin;
+    vector<long long> ele;
+    long long tempmin;
     int ind;
 
     MinStack() {
-        tempmin=INT_MAX;
+        tempmin=LLONG_MAX;;
         ind=-1;
     }
     
+ 
     void push(int val) {
-        ind++;
-        ele.push_back(val);
-        tempmin=min(val,tempmin);
-        currmin.push_back(tempmin);
+        if(ele.empty()){
+            ele.push_back(0); 
+            tempmin = val; 
+        } 
+        else{
+            ele.push_back(val - tempmin); 
+            if(val < tempmin){
+                tempmin = val; 
+            }
+        }
     }
     
     void pop() {
+        if (ele.empty()) return;
+        
+        long long top = ele.back();
         ele.pop_back();
-        currmin.pop_back();
-        ind--;
-        if(ind<0){
-            tempmin=INT_MAX;
-        }
-        else{
-            tempmin = currmin[ind];
+        
+        if(top < 0){
+            tempmin = tempmin - top; 
         }
     }
     
-    int top() {
-        return ele[ind];
+    int top(){
+
+        long long top = ele.back();
+        
+        if(top > 0){
+            return top + tempmin; 
+        }
+        else{
+            return tempmin; 
+        }
     }
     
     int getMin() {
-        return currmin[ind];
+        return tempmin;
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
