e69326b044cd2be693e860d50ddd15b38037688a
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-13 08:59:46 +0530

    Time: 5 ms (34.48%), Space: 17.6 MB (7.78%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,58 +1,73 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Approach - 1 : Bruteforce using horizontal distancce and pair
+
+
+// class Solution {
+
+// private:
+
+//     void traverseLeft(TreeNode *root,vector<pair<int,int>> &arr, int dist){
+//         if(!root) return;
+
+//         arr.push_back({root->val,dist});
+
+//         traverseLeft(root->left,arr,dist-1);
+//         traverseLeft(root->right,arr,dist+1);
+//     }
+
+//     void traverseRight(TreeNode *root,vector<pair<int,int>> &arr, int dist){
+//         if(!root) return;
+
+//         arr.push_back({root->val,dist});
+
+//         traverseRight(root->right,arr,dist+1);
+//         traverseRight(root->left,arr,dist-1);
+//     }
+
+// public:
+//     bool isSymmetric(TreeNode* root) {
+
+//         vector<pair<int,int>> left;
+//         vector<pair<int,int>> right;
+
+//         traverseLeft(root,left,0);
+//         traverseRight(root,right,0);
+
+//         int n = left.size();
+
+//         if(n!=right.size()) return false;
+
+//         for(int i=0;i<n;i++){ 
+//             if(left[i].first!=right[i].first)  return false;
+//             else if(-left[i].second!=right[i].second) return false;   
+//         }
+
+//         return true;
+
+//     }
+// };
+
+
+
+// Approach - 2 
+
+
 class Solution {
 
 private:
 
-    void traverseLeft(TreeNode *root,vector<pair<int,int>> &arr, int dist){
-        if(!root) return;
 
-        arr.push_back({root->val,dist});
+    bool helper(TreeNode *left, TreeNode *right){
+        if(!left || !right) return left==right;
+        
+        if(left->val!=right->val) return false;
 
-        traverseLeft(root->left,arr,dist-1);
-        traverseLeft(root->right,arr,dist+1);
-    }
-
-    void traverseRight(TreeNode *root,vector<pair<int,int>> &arr, int dist){
-        if(!root) return;
-
-        arr.push_back({root->val,dist});
-
-        traverseRight(root->right,arr,dist+1);
-        traverseRight(root->left,arr,dist-1);
+        return helper(left->left,right->right) && helper(left->right,right->left);
     }
 
 public:
     bool isSymmetric(TreeNode* root) {
 
-        vector<pair<int,int>> left;
-        vector<pair<int,int>> right;
-
-        traverseLeft(root,left,0);
-        traverseRight(root,right,0);
-
-        int n = left.size();
-
-        if(n!=right.size()) return false;
-
-
-        for(int i=0;i<n;i++){
-            
-            if(left[i].first!=right[i].first)  return false;
-            else if(-left[i].second!=right[i].second) return false;  
-            
-        }
-
-        return true;
+        return !root || helper(root->left,root->right);
 
     }
 };
