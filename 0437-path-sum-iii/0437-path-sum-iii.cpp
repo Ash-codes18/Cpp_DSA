97285d9d1152deb71918cf68115580dab9d30b4a
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-13 20:32:06 +0530

    Time: 18 ms (51.06%), Space: 17.8 MB (55.25%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,14 +1,63 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Approach - 1 : BFS+DFS -> DFS on every node using BFS
+
+
+// class Solution {
+
+// private:
+//     void dfs(TreeNode* root, int x, long long sum, long long &count){
+//         if(!root) return;
+
+//         sum+=root->val;
+//         // cout<<sum<<" ";
+
+//         if(sum==x) count++;
+
+//         dfs(root->left,x,sum,count);
+//         dfs(root->right,x,sum,count);
+//     }
+
+//     void bfs(TreeNode* root, long long x, long long &count){
+//         if(!root) return;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while(!q.empty()){
+
+//             for(int i=0;i<q.size();i++){                
+//                 TreeNode* ele = q.front();
+//                 q.pop();
+//                 // cout<<ele->val<<" : ";
+//                 dfs(ele,x,0,count);
+//                 // cout<<endl;
+
+//                 if(ele->left){
+//                     q.push(ele->left);
+//                 }
+//                 if(ele->right){
+//                     q.push(ele->right);
+//                 }
+//             }
+//         }
+
+//     }
+
+// public:
+//     int pathSum(TreeNode* root, int targetSum) {
+        
+//         long long count = 0;
+//         bfs(root,targetSum,count);
+//         return count;
+
+//     }
+// };
+
+
+
+
+// Approach - 2  : DFS on every node using DFS
+
+
 class Solution {
 
 private:
@@ -16,7 +65,6 @@ private:
         if(!root) return;
 
         sum+=root->val;
-        // cout<<sum<<" ";
 
         if(sum==x) count++;
 
@@ -24,37 +72,17 @@ private:
         dfs(root->right,x,sum,count);
     }
 
-    void bfs(TreeNode* root, long long x, long long &count){
-        if(!root) return;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-
-            for(int i=0;i<q.size();i++){                
-                TreeNode* ele = q.front();
-                q.pop();
-                // cout<<ele->val<<" : ";
-                dfs(ele,x,0,count);
-                // cout<<endl;
-
-                if(ele->left){
-                    q.push(ele->left);
-                }
-                if(ele->right){
-                    q.push(ele->right);
-                }
-            }
-        }
-
-    }
 
 public:
+    long long count = 0;
     int pathSum(TreeNode* root, int targetSum) {
         
-        long long count = 0;
-        bfs(root,targetSum,count);
+        if(root){
+            dfs(root,targetSum,0,count);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+        }
+          
         return count;
 
     }
