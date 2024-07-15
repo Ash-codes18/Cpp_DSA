8a2bb90190c7b7ed2e48faff942872579a2b23df
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-15 08:27:38 +0530

    Time: 714 ms (51.09%), Space: 298.1 MB (26.2%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -1,20 +1,12 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& arr) {
         
-        map<int,bool> mp;
-        map<int,TreeNode*> nodes;
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+        unordered_map<int,bool> mp;
+        unordered_map<int,TreeNode*> nodes;
 
 
         for(auto i : arr){
