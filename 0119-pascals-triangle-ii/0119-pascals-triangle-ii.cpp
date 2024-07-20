cd7ae9064e53ee40b6a58ad2b8834c4d5e50957d
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-20 10:58:25 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.36%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,19 +1,57 @@
-class Solution {
-public:
-    vector<int> getRow(int rowIndex) {
-        vector<vector<int>> ans;
+// Approach -1 -> BruteForcce, Generating the entire pascal's triangel
 
-        for(int i=0;i<=rowIndex;i++){
-            vector<int> temp;
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i) temp.push_back(1);
-                else{
-                    temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
-                }
-            }
-            ans.push_back(temp);
+
+// class Solution {
+// public:
+//     vector<int> getRow(int rowIndex) {
+//         vector<vector<int>> ans;
+
+//         for(int i=0;i<=rowIndex;i++){
+//             vector<int> temp;
+//             for(int j=0;j<=i;j++){
+//                 if(j==0 || j==i) temp.push_back(1);
+//                 else{
+//                     temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
+//                 }
+//             }
+//             ans.push_back(temp);
+//         }
+
+//     return ans[rowIndex];
+//     }
+// };
+
+
+
+// Approach -2 -> Using Math
+
+
+class Solution {
+
+private:
+    int nCr(int n, int r){
+        if(r==0 || r==n) return 1;
+
+        long long res = 1;
+
+        for(int i=0;i<r;i++){
+            res*=(n-i);
+            res/=(i+1);
+        } 
+
+        return res;
+    }
+
+
+public:
+    vector<int> getRow(int n) {
+
+        vector<int> ans;
+
+        for(int r=0;r<=n;r++){
+            ans.push_back(nCr(n,r));
         }
 
-    return ans[rowIndex];
+        return ans;
     }
 };
