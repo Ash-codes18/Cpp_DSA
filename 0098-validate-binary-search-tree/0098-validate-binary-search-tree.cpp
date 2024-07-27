fdec73f0fe1fe657c478dd91388e9209d091d748
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-27 11:31:54 +0530

    Time: 12 ms (22.29%), Space: 20.4 MB (32.52%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,25 +1,48 @@
+// Approach - 1 - inorder traversal
+
+// class Solution {
+// private:
+//     void dfs(TreeNode* root, long &prev, bool &ans) {
+//         if (!root || !ans) return;
+
+//         dfs(root->left, prev, ans);
+        
+//         if (root->val <= prev) {
+//             ans = false;
+//             return;
+//         }
+//         prev = root->val;
+
+//         dfs(root->right, prev, ans);
+//     }
+
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         if (!root) return true;
+//         long prev = LONG_MIN;
+//         bool ans = true;
+//         dfs(root, prev, ans);
+//         return ans;
+//     }
+// };
+
+
+
+// Approach - 2 : Using Range
+
+
 class Solution {
 private:
-    void dfs(TreeNode* root, long &prev, bool &ans) {
-        if (!root || !ans) return;
+    bool dfs(TreeNode* root, long min, long max) {
+        if (!root) return true;
 
-        dfs(root->left, prev, ans);
-        
-        if (root->val <= prev) {
-            ans = false;
-            return;
-        }
-        prev = root->val;
+        if(root->val>=max || root->val<=min) return false;
 
-        dfs(root->right, prev, ans);
+        return dfs(root->left,min,root->val) && dfs(root->right,root->val,max);
     }
 
 public:
     bool isValidBST(TreeNode* root) {
-        if (!root) return true;
-        long prev = LONG_MIN;
-        bool ans = true;
-        dfs(root, prev, ans);
-        return ans;
+        return dfs(root,LONG_MIN,LONG_MAX);
     }
 };
