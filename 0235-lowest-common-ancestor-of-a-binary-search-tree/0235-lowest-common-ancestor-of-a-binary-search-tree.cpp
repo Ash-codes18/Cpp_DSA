1494c0f89631cc4dcc9f95e5be922c7df9a20ec0
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-07-27 17:37:53 +0530

    Time: 22 ms (68.39%), Space: 22.1 MB (6.73%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,28 +1,50 @@
-class Solution {
-private:
-    void path(TreeNode* root, TreeNode* &p, vector<TreeNode*> &a, vector<TreeNode*> temp){
-        if(!root || !a.empty()) return;
+// Naive 
+
+// class Solution {
+// private:
+//     void path(TreeNode* root, TreeNode* &p, vector<TreeNode*> &a, vector<TreeNode*> temp){
+//         if(!root || !a.empty()) return;
             
-        temp.push_back(root);
-        if(root==p) a=temp; 
+//         temp.push_back(root);
+//         if(root==p) a=temp; 
 
-        if(root->val>p->val) path(root->left,p,a,temp);
-        else path(root->right,p,a,temp);
-    }
+//         if(root->val>p->val) path(root->left,p,a,temp);
+//         else path(root->right,p,a,temp);
+//     }
 
 
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         vector<TreeNode*> a,b,temp;
+//         path(root,p,a,temp);
+//         path(root,q,b,temp);
+
+//         TreeNode* ans;
+
+//         for(int i=0;i<a.size() && i<b.size();i++){
+//             if(a[i] == b[i]) ans = a[i];
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+// Better
+
+
+class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> a,b,temp;
-        path(root,p,a,temp);
-        path(root,q,b,temp);
 
-        TreeNode* ans;
-
-        for(int i=0;i<a.size() && i<b.size();i++){
-            if(a[i] == b[i]) ans = a[i];
+        while(1){
+            if(p->val<root->val && q->val<root->val) root = root->left;
+            else if(p->val>root->val && q->val>root->val) root = root->right;
+            else return root;
         }
 
-        return ans;
+        return root;
     }
 };
