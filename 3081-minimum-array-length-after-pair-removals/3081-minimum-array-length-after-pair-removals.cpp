e3634daec2ab884418238825311af57bed241fef
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-08-03 09:42:59 +0530

    Time: 106 ms (95.47%), Space: 152 MB (68.32%) - LeetHub

--- 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.cpp
+++ 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int minLengthAfterRemovals(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
         
-        int n = nums.size();
-        int s = 0;
-        int m =  n/2;
+        int n = nums.size(), s = 0, m =  n/2;
+        int ans = n;
         while(s<n/2 && m<n){
             if(nums[s]<nums[m]){
-                nums[s]=-1;
-                nums[m]=-1;
+                ans-=2;
                 s++;
                 m++;
             }
             else m++;
-        }
-
-        int ans = 0;
-
-        for(int i : nums){
-            if(i!=-1) ans++;
         }
 
         return ans;
