483ca3f0f595ba6510f0ffbc8ca560e3a65b5844
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-08-04 16:26:24 +0530

    Time: 94 ms (55.51%), Space: 61.9 MB (53.08%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,33 +1,52 @@
 // Approach - 1 : Using maps
 
+// class Solution {
+// public:
+//     int maxOperations(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int ans = 0;
+
+//         unordered_map<int,int> mp;
+//         for(int &i : nums) mp[i]++;
+
+//         for(int &i : nums){
+//             if(mp[i]){
+//                 mp[i]--;
+//                 if(mp[k-i]){
+//                     ans++;
+//                     mp[k-i]--;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+// Approach -2 Using Sorting & 2 pointers
+
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         int n = nums.size();
+        int i = 0;
+        int j = n-1;
         int ans = 0;
 
-        unordered_map<int,int> mp;
-        for(int &i : nums) mp[i]++;
+        sort(nums.begin(),nums.end());      
 
-        for(int &i : nums){
-            if(mp[i]){
-                mp[i]--;
-                if(mp[k-i]){
-                    ans++;
-                    mp[k-i]--;
-                }
+        while(i<j){
+            int sum = nums[i]+nums[j];
+            if(sum==k){
+                ans++;
+                i++;
+                j--;
             }
+            else if(sum<k) i++;
+            else j--;
         }
 
         return ans;
     }
 };
-
-// Approach -2 Using Sorting & 2 pointers
-
-// class Solution {
-// public:
-//     int maxOperations(vector<int>& nums, int k) {
-        
-//     }
-// };
