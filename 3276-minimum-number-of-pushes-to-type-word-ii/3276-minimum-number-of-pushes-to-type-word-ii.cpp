2e138eeabe62c2d8e007f413377873e93b93e55a
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-08-06 08:46:57 +0530

    Time: 70 ms (70.7%), Space: 24.7 MB (66.15%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,26 +1,46 @@
+// Approach -1 : Very very dumb
+
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         unordered_map<char,int> mp;
+//         for(char i : word) mp[i]++;
+
+//         if(mp.size()<=8) return word.length();
+
+//         vector<pair<int,char>> arr;
+//         for(auto i : mp) arr.push_back({i.second,i.first});
+//         sort(arr.begin(),arr.end());
+//         reverse(arr.begin(),arr.end());
+
+//         int ans = 0;
+
+//         for(int i=0;i<arr.size();i++){
+//             if(i<8) ans+=arr[i].first;
+//             else{
+//                 ans+=(arr[i].first)*((i/8)+1);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+// Approach - 2 : How come are you such an idiot?
+
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char,int> mp;
-        for(char i : word) mp[i]++;
+        vector<int> arr(26);
+        for(char i : word) arr[i-'a']++;
 
-        if(mp.size()<=8) return word.length();
-
-        vector<pair<int,char>> arr;
-        for(auto i : mp) arr.push_back({i.second,i.first});
-        sort(arr.begin(),arr.end());
-        reverse(arr.begin(),arr.end());
-
-        // for(auto i : arr) cout<<i.second<<" : "<<i.first<<endl;
+        sort(arr.begin(),arr.end(),greater<int>());
 
         int ans = 0;
 
-        for(int i=0;i<arr.size();i++){
-            if(i<8) ans+=arr[i].first;
-            else{
-                ans+=(arr[i].first)*((i/8)+1);
-            }
-            cout<<ans<<endl;
+        for(int i=0;i<26;i++){
+            ans+=((i/8)+1)*arr[i];
         }
 
         return ans;
